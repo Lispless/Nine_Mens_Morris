1d29d85e899b2f4e1f253cce8631172d529261b5
Garrett Amini <kadjar@gmail.com>
2014-04-06 11:30:09 -0700

    adding public, bootstrapping app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-var express = require('express');
+var express = require('express'),
+    http = require('http'),
+    path = require('path');
 
 var app = module.exports = express();
 
+var server = require('http').createServer(app);
+
+app.set('port', process.env.PORT || 3000);
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+server.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
