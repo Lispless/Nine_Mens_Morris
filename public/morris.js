6bdd3cc67339c7ce9f2eb6776c9b03c22a67e441
Lispless <celfrost@hotmail.com>
2014-04-06 13:46:23 -0700

    on

--- public/morris.js
+++ public/morris.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
 
-$(".checker").on("click", function(test) {
-$(this).toggleClass('red')
-} )
+// $(".checker").on("click", function(test) {
+// $(this).toggleClass('red')
+// } )
 
-$(".checker").draggable();
-});
+// $(".checker").draggable({ snap: ".slot", snapMode: "Inner", snapTolerence: "5px" });
+
+$(".slot").on("click", function() {
+  $(this).find(".blackpiece").toggleClass("on")
+})
+
+ });
+
