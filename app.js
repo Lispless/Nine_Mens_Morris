a50e115bb93d92a86fef9aa6f43df6f58bb8803c
Lispless <celfrost@hotmail.com>
2014-04-06 11:21:30 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+
+var app = module.exports = express();
+
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
